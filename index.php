985a610e40e97435ed1e62e9b873e5629ca36ff7
Yelley <yel531129@outlook.com>
2021-10-20 13:10:03 +0700

    yel

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include "connect.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <ul class="links" id="myLinks">
+            <li><strong><a href="#">GYP Dessert</a></li></strong>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Beverage</a></li>
+            <li><a href="#">Bakery</a></li>         
+            <li class="right"><a href="#">register</a></li>
+            <li class="right"><a href="#">login</a></li>
+            <li class="icon">
+                <a href="#" onclick="myFunction()">&#9776;</a>
+            </li>
+        </ul>
+    </nav>
+</body>
+<script src="./grid.js"></script>
+</html>
