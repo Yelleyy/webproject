e76098a22475fb092de1a3e4e67806ab848d864a
Yelley <yel531129@outlook.com>
2021-10-26 20:52:37 +0700

    yel

--- address_add.php
+++ address_add.php
@@ -19,14 +19,12 @@ include("tools.php");
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dependencies/JQL.min.js"></script>
 <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dependencies/typeahead.bundle.js"></script>
-
 <link rel="stylesheet" href="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.css">
 <script type="text/javascript" src="https://earthchie.github.io/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.js"></script>
 <style>
-	* {
-		margin: 0;
-		padding: 0;
-		box-sizing: border-box;
+	.center {
+		margin: 0 auto;
+		max-width: 1280px;
 	}
 
 	.container {
@@ -47,46 +45,61 @@ include("tools.php");
 
 	.txt {
 		width: 100%;
-		background: #f2f2f2;
+		background: #f2afb4;
 		outline: none;
 		border: none;
 		padding: 10px;
 		border-radius: 10px;
 		font-size: 1rem;
 	}
+
+	.btn {
+		cursor: pointer;
+		background: #f2afb4;
+		padding: 8px;
+		border-radius: 13px;
+		box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+		font-size: 1rem;
+		font-weight: bold;
+		transition: box-shadow linear 0.4s;
+	}
+
+	.btn:hover {
+		-webkit-box-shadow: none;
+		box-shadow: none;
+	}
 </style>
-
-<head>
-
-</head>
 
 <body>
 	<br><br><br>
-	<div class="container">
-		<div class="card-header">
-			<h4 class="card-title">เพิ่มที่อยู่</h4>
-		</div>
-		<div class="container">
-			<h2>Thailand.js</h2>
+
+	<form action="address_adddb.php" method="post">
+		<div class="container center">
+			<h2>ฟอร์มเพิ่มที่อยู่</h2>
+			<div class="form-control">
+				<span>รายละเอียดบ้าน</span>
+				<textarea name="detail" type="text" class="txt" placeholder="รายละเอียดบ้าน"></textarea>
+			</div>
 			<div class="form-control">
 				<span>ตำบล/แขวง</span>
-				<input id="district" type="text" class="txt" placeholder="ตำบล">
+				<input id="district" name="district" type="text" class="txt" placeholder="ตำบล">
 			</div>
 			<div class="form-control">
 				<span>อำเภอ/เขต</span>
-				<input id="amphoe" type="text" class="txt" placeholder="อำเภอ">
+				<input id="amphoe" name="amphoe" type="text" class="txt" placeholder="อำเภอ">
 			</div>
 			<div class="form-control">
 				<span>จังหวัด</span>
-				<input id="province" type="text" class="txt" placeholder="จังหวัด">
+				<input id="province" name="province" type="text" class="txt" placeholder="จังหวัด">
 			</div>
 			<div class="form-control">
 				<span>รหัสไปรษณีย์</span>
-				<input id="zipcode" type="text" class="txt" placeholder="รหัสไปรษณีย์">
+				<input id="zipcode" name="zipcode" type="text" class="txt" placeholder="รหัสไปรษณีย์">
 			</div>
+			<button type="submit" class="btn">เพิ่มที่อยู่</button>
 		</div>
+	</form>
 
-	</div>
 </body>
 <script>
 	$.Thailand({
