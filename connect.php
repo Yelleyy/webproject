f33ec372328fd5def5495cd109f1a7d7b931bd23
mavbxxm <ggguigee@gmail.com>
2021-11-12 17:17:01 +0700

    1

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo=new PDO("mysql:host=localhost;dbname=gypdb;charset=utf8","root","");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "เกิดข้อผิดพลาด : ".$e->getMessage();
-}
-?>
