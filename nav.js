f45681e959a72cea67b8dfd789fe3388a5470d7d
mavbxxm <ggguigee@gmail.com>
2021-11-12 18:08:22 +0700

    1

--- nav.js
+++
@@ -1,8 +0,0 @@
-function nav() {
-    var x = document.getElementById("nav");
-    if (x.className === "links") {
-        x.className += " responsive";
-    } else {
-        x.className = "links";
-    }
-}
