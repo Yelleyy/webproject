53eb239bec862a38ae873b96470b6b22d56f9a8c
Yelley <yel531129@outlook.com>
2021-11-12 13:40:18 +0700

    yel

---
+++ listfood_edit.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['email']);
+    header('location: login.php');
+}
+if (!isset($_SESSION['email'])) {
+    echo "<script type='text/javascript'>";
+    echo "alert('กรุณาเข้าสู่ระบบ');";
+    echo "window.location = 'login.php'; ";
+    echo "</script>";
+}
+include("connect.php");
+$ID_Food = $_REQUEST["ID"];
+$stmt1 = $pdo->prepare("SELECT * FROM stock WHERE ID_Food = '$ID_Food'");
+$stmt1->execute();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body >
+    <br><br><br><br>
+    <form action='listfood_update.php?ID=<?php echo "$ID_Food"; ?>' method="post" enctype="multipart/form-data">
+        <?php $row1 = $stmt1->fetch(); ?>
+        <div class="center"  style="font-size: 1.5rem;">
+            <div>
+                <label>ชื่อ</label>
+                <input type="text" name="Food_Name" value=<?php echo $row1[1]; ?>>
+            </div>
+            <div>
+                <label>ราคา</label>
+                <input type="text" name="price" value=<?php echo $row1[3]; ?>>
+            </div>
+            <div>
+                <label>รูป</label>
+                <input type="text" name="PicFood" value=<?php echo $row1[4]; ?>>
+            </div>
+            <button type="submit" name="upload">แก้ไข</button>
+    </form>
+
+</body>
+
+</html>
---
+++ listfood_update.php
@@ -0,0 +1,27 @@
+<meta charset="UTF-8" />
+<?php
+session_start();
+include('connect.php');
+
+$Food_Name = $_POST['Food_Name'];
+$amount =  $_POST['amount'];
+$price =  $_POST['price'];
+$PicFood =  $_POST['PicFood'];
+$ID_Food = $_REQUEST["ID"];
+
+$stmt = $pdo->prepare("UPDATE stock SET Food_Name='$Food_Name',amount='$amount',price='$price',PicFood='$PicFood'
+ WHERE ID_Food=$ID_Food ");
+$stmt->execute();
+
+if ($stmt) {
+    echo "<script type='text/javascript'>";
+    echo  "alert('แก้ไขเรียบร้อยเรียบร้อย');";
+    echo "window.location='listfood.php';";
+    echo "</script>";
+} else {
+    echo "<script type='text/javascript'>";
+    echo "window.location='listfood.php';";
+    echo "</script>";
+}
+mysqli_close($con);
+?>
